a8a8de7bd7c21c4de152fa871c94676bc40c5e13
Michael Gerov <mhgerov@gmail.com>
2020-07-23 18:06:41 -0400

    integration-test runs first case

---
+++ integration-tests/README.md
@@ -0,0 +1,1 @@
+https://www.baeldung.com/junit-tests-run-programmatically-from-java
---
+++ integration-tests/src/main/java/site/seedmonkey/example/petstore/integrationtests/JunitStartupApplicationListener.java
@@ -0,0 +1,30 @@
+package site.seedmonkey.example.petstore.integrationtests;
+
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import site.seedmonkey.example.petstore.integrationtests.tests.IntegrationTestsApplicationTests;
+
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+
+@Component
+public class JunitStartupApplicationListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        System.out.println("Starting Tests!");
+        SummaryGeneratingListener listener = new SummaryGeneratingListener();
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                .selectors(selectClass(IntegrationTestsApplicationTests.class))
+                .build();
+        Launcher launcher = LauncherFactory.create();
+        launcher.discover(request);
+        launcher.registerTestExecutionListeners(listener);
+        launcher.execute(request);
+    }
+}
